1bcf182da2825bae1ce681bf0613a10d06e59afb
Maksim Nesterenko <maksim.nesterenko@forsta.com>
2023-01-14 16:08:35 +0100

    feat: add basic location input

--- src/features/dashboard/dashboard-page.tsx
+++ src/features/dashboard/dashboard-page.tsx
@@ -1,19 +1,42 @@
 import { WeatherList } from "./weather-list";
+import { FormEvent, useCallback, useState } from "react";
 
 // gets saved user locations or gives default list
-const useLocationsList = (): string[] => {
-  return ["london", "berlin"];
+const useLocationsList = () => {
+  const [locations, setLocations] = useState(["london", "berlin"]);
+
+  return {
+    locations,
+    addLocation: useCallback((newLocation: string) => {
+      const newLocationLowercase = newLocation.toLowerCase();
+      if (!locations.includes(newLocationLowercase)) {
+        setLocations(locations.concat(newLocationLowercase));
+      }
+    }, locations),
+  };
 };
 
 /**
  * Main page that displays all the locations that users added to track weather for
  */
 export function DashboardPage() {
-  const locations = useLocationsList();
+  const { locations, addLocation } = useLocationsList();
+  const [newLocation, setNewLocation] = useState("");
+
+  const handleNewLocationSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    addLocation(newLocation);
+  };
 
   return (
     <div>
-      <pre>{JSON.stringify(locations)}</pre>
+      <form onSubmit={handleNewLocationSubmit}>
+        <input
+          value={newLocation}
+          onChange={(e) => setNewLocation(e.target.value)}
+          placeholder="Add new location"
+        />
+      </form>
       <WeatherList locations={locations}></WeatherList>
     </div>
   );
