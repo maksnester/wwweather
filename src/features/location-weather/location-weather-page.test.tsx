dd02d2b902aaf7417911532abc688e28a652315e
Maksim Nesterenko <maksim.nesterenko@forsta.com>
2023-01-15 20:27:56 +0100

    test: finish location-weather-page cases

--- src/features/location-weather/location-weather-page.test.tsx
+++ src/features/location-weather/location-weather-page.test.tsx
@@ -1,20 +1,90 @@
 import { describe, it, expect } from "vitest";
-import { screen } from "@testing-library/react";
+import { screen, waitFor } from "@testing-library/react";
 import { createMemoryRouter, RouterProvider } from "react-router-dom";
 
 import { render } from "../../../test-utils/render";
 import { routes } from "../routes";
 
-describe("something truthy and falsy", () => {
+import { rest } from "msw";
+import { server } from "../../../test-utils/mocks/server";
+import { geoDirectHandler } from "../../../test-utils/mocks/geo-direct-handler";
+import { dataWeatherHandler } from "../../../test-utils/mocks/data-weather-handler";
+
+describe("location-weather-page", () => {
   it("renders weather details for Khabarovsk city", async () => {
+    server.use(geoDirectHandler, dataWeatherHandler);
+
     const router = createMemoryRouter(routes, {
       initialEntries: ["/khabarovsk"],
     });
 
     render(<RouterProvider router={router} />);
 
-    expect(await screen.findByText("khabarovsk")).toBeInTheDocument();
+    await waitFor(() => {
+      expect(screen.queryByText("Loading...")).not.toBeInTheDocument();
+    });
 
-    screen.debug();
+    expect(screen.getByRole("heading", "khabarovsk")).toBeInTheDocument();
+
+    expect(screen.getByTestId("weather-type")).toHaveTextContent("Clear");
+    expect(screen.getByTestId("temperature")).toHaveTextContent("-24");
+    expect(screen.getByTestId("sunrise-time")).toHaveTextContent("8:46 AM");
+    expect(screen.getByTestId("sunset-time")).toHaveTextContent("5:31 PM");
+  });
+
+  it("renders an error message for unknown location", async () => {
+    server.use(
+      rest.get(
+        "https://api.openweathermap.org/geo/1.0/direct",
+        (req, res, ctx) => {
+          // actual API reacts the same way
+          return res(ctx.status(200), ctx.json([]));
+        }
+      )
+    );
+
+    const router = createMemoryRouter(routes, {
+      initialEntries: ["/something-that-totally-makes-no-sense"],
+    });
+
+    render(<RouterProvider router={router} />);
+
+    await waitFor(() => {
+      expect(screen.queryByText("Loading...")).not.toBeInTheDocument();
+    });
+
+    expect(
+      screen.getByRole("heading", "something-that-totally-makes-no-sense")
+    ).toBeInTheDocument();
+
+    expect(
+      screen.getByText(/Can't find any locations with name/i)
+    ).toBeInTheDocument();
+  });
+
+  it("renders an error message for server error", async () => {
+    server.use(
+      rest.get(
+        "https://api.openweathermap.org/geo/1.0/direct",
+        (req, res, ctx) => {
+          // actual API reacts the same way
+          return res(ctx.status(500), ctx.json("server goes brrrr"));
+        }
+      )
+    );
+
+    const router = createMemoryRouter(routes, {
+      initialEntries: ["/tokyo"],
+    });
+
+    render(<RouterProvider router={router} />);
+
+    await waitFor(() => {
+      expect(screen.queryByText("Loading...")).not.toBeInTheDocument();
+    });
+
+    expect(screen.getByRole("heading", "tokyo")).toBeInTheDocument();
+
+    expect(screen.getByText(/server goes brrrr/i)).toBeInTheDocument();
   });
 });
